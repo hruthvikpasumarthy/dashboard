d211fa7707979701c36ef91882a4ec2f5b532852
hruthvikpasumarthy <59084097+hruthvikpasumarthy@users.noreply.github.com>
2021-04-27 18:25:33 +0530

    Update front.js

--- front.js
+++ front.js
@@ -59,8 +59,8 @@ $('div').html(table);
         // instantiate a headers object
         var myHeaders = new Headers();
         // add content type header to object
-        alert("id1");
-        console.log("id1");
+        alert(id1);
+        console.log(id1);
         myHeaders.append("Content-Type", "application/json");
         // using built in JSON utility package turn object to string and store in a variable
         var raw = JSON.stringify({"asset":asset});
